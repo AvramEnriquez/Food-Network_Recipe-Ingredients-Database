8f1e95ad4c874bda645b3a9a7415ac49dad973bd
AvramEnriquez <120682270+AvramEnriquez@users.noreply.github.com>
2023-01-01 23:17:15 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,19 +1,43 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-url = "https://www.foodnetwork.com/recipes/tyler-florence/fajitas-recipe-1906480"
+url = "https://www.foodnetwork.com/recipes/haggis-recipe-2126047"
 
 def scraper(url):
     # Request HTML for URL then parse it
     result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     
-    # Look for Food Network's ingredients checkbox by looking for inputs that contain the following conditions 
+    # Look for Food Network's ingredients checklist by looking for inputs that contain the following conditions 
     shopping_list = soup.find_all("input", {"class" : "o-Ingredients__a-Ingredient--Checkbox"}, {"type" : "checkbox"})
 
-    ingredients = []
-    ingredients = [item["value"] for item in shopping_list[1:]]  # Item "value" contains ingredient + quantity
+    # Item "value" contains ingredient + quantity
+    ingredients_quantity = [item["value"] for item in shopping_list[1:]]
 
-    return ingredients
+    return ingredients_quantity
 
-print(scraper(url))
+ingredients_quantity = scraper(url)
+
+print(ingredients_quantity)
+
+measurements = ([' ml ',' mL ',' milliliter ',' milliliters ',' millilitre ',' millilitres ',
+' cc ',' l ',' L ',' liter ',' liters ',' litre ',' litres ',' teaspoon ',' teaspoons ',
+' tablespoon ',' tablespoons ',' T ',' tbl ',' tbs ',' tbsp ',' fluid ounce ',' fluid ounces ',
+' fl oz ',' gill ',' gills ',' cup ',' cups ',' c ',' pint ',' pints ',' p ',' pt ',' pts ',
+' fl pt ',' fl pts ',' quart ',' quarts ',' q ',' qt ',' fl qt ',' fl qts ',' gallon ',
+' gallons ',' g ',' gal ',' gals ',' mg ',' milligram ',' milligrams ',' milligramme ',
+' milligrammes ',' g ',' gs ',' gram ',' grams ',' gramme ',' grammes ',' kg ',' kgs ',
+' kilogram ',' kilograms ',' kilogramme ',' kilogrammes ',' pound ',' pounds ',' lb ',' lbs ',
+' # ',' ounce ',' ounces ',' oz '])
+
+for item in ingredients_quantity:
+    # If int exists in string then pull int
+    if any(char.isdigit() for char in item):
+        for unit in measurements:
+            if unit in item:
+                quantity = (re.search(r'\d+', item).group()) + unit
+                print(quantity)
+    else:
+        # Else put 0
+        print(0)
