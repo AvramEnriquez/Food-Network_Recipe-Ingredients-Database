921192ad27bd3f58279f5975ed5d506c3d0f631d
AvramEnriquez <120682270+AvramEnriquez@users.noreply.github.com>
2023-01-09 15:15:21 -0500

    Update scraper and WIP for databse

--- ingredient_database.py
+++ ingredient_database.py
@@ -1,4 +1,4 @@
-from scraper import ingredient_list
+from scraper import ingredient_list, recipe_name
 import psycopg2
 
 """CONNECTING TO DATABASE"""
@@ -20,7 +20,7 @@ try:
 except:
     print("Database failed to connect.")
 
-# Create recipe, ingredients, and quantities tables
+# Create recipe and ingredient tables
 tables = ['recipe_table','ingredient_table']
 for table_name in tables:
     try:
@@ -37,14 +37,15 @@ for table_name in tables:
         print(f"Table {table_name} already exists.")
     conn.commit()  # Commit the change
 
+# Create quantity table
 quantity_table = 'quantity_table'
 try:
     cur.execute(f"""
         CREATE TABLE {quantity_table} (
-            "unit" VARCHAR,
-            "quantity#" VARHCAR,
-            "recipe_id" INT,
-            "ingredient_id" INT
+            recipe_id INT,
+            ingredient_id INT,
+            quantity VARCHAR,
+            unit VARCHAR
         );
         """)
     print(f"Table {quantity_table} created successfully.")
@@ -53,17 +54,45 @@ except psycopg2.errors.DuplicateTable:
 conn.commit()  # Commit the change
 
 def insert():
+    cur.execute(f"""
+            INSERT INTO recipe_table (
+                "name"
+            )
+            VALUES (
+                '{recipe_name}'
+            );
+            """)
+    conn.commit()
+
     for list in ingredient_list:
-        name = list[1]
+        quantity = list[0]
+        unit = list[1]
+        ingredient = list[2]
     
         cur.execute(f"""
             INSERT INTO ingredient_table (
                 "name"
             )
             VALUES (
-                '{name}'
+                '{ingredient}'
             );
             """)
-        conn.commit() 
+        conn.commit()
+
+        cur.execute(f"""
+            INSERT INTO quantity_table (
+                recipe_id,
+                ingredient_id,
+                quantity,
+                unit
+            )
+            VALUES (
+                (SELECT id FROM recipe_table),
+                (SELECT id FROM ingredient_table),
+                '{quantity}',
+                '{unit}'
+            );
+            """)
+        conn.commit()
 
 table = insert()
--- scraper.py
+++ scraper.py
@@ -32,14 +32,18 @@ def scraper(url):
     # Request HTML for URL then parse it
     result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
-    
-    # Look for Food Network's ingredients checklist by looking for inputs that contain the following conditions 
-    shopping_list = soup.find_all("input", {"class" : "o-Ingredients__a-Ingredient--Checkbox"}, {"type" : "checkbox"})
 
+    # Look for Food Network's recipe name by looking for meta element that contains the following condition
+    scraped_recipe_name = soup.find("meta", {"property" : "og:title"})
+    # Item content contains name
+    recipe_name = scraped_recipe_name["content"]
+
+    # Look for Food Network's ingredients checklist by looking for input elements that contain the following conditions 
+    shopping_list = soup.find_all("input", {"class" : "o-Ingredients__a-Ingredient--Checkbox"}, {"type" : "checkbox"})
     # Item "value" contains ingredient + quantity
     scraped_ingred_quant = [item["value"] for item in shopping_list[1:]]
 
-    return scraped_ingred_quant
+    return recipe_name, scraped_ingred_quant
 
 def cleanup(scraped_ingred_quant):
     ingredient_list = []
@@ -60,7 +64,7 @@ def cleanup(scraped_ingred_quant):
                     if unit in item:
                         # If multiple units exist in one string, iterate with earlier unit
                         # e.g. '5 tablespoons plus 3 ounces melted butter' <- Why do people do this?
-                        # Keep iterating until it earliest unit is found
+                        # Keep iterating until earliest unit is found
                         if item.index(unit) < item.index(x_unit) or item.index(x_unit) == 0:
                             x_unit = unit
                             list = item.split(x_unit, 1)
@@ -74,9 +78,9 @@ def cleanup(scraped_ingred_quant):
                                 # Convert any fractions to floats
                                 elif '/' in list[0]:
                                     list[0] = str(round(float(Fraction(list[0])), 2))
-                                list[0] += x_unit
+                                list.insert(1, x_unit)
                             except ValueError:
-                                # ValueError occurs if multiple units exist in one string and algorithm iterates that later one
+                                # ValueError occurs if multiple units exist in one string and algorithm iterates the later one
                                 # Not sure how else to work around this
                                 continue
             else:
@@ -85,15 +89,17 @@ def cleanup(scraped_ingred_quant):
                 # and split accordingly
                 list = item.split(num, 1)
                 list[0] += num
+                list.insert(1, '')
             # Remove leading/trailing whitespace
             list = [s.strip() for s in list]
         else:
-            list = ['', item]
+            list = ['', '', item]
         ingredient_list.append(list)
     return ingredient_list
 
-scraped_ingred_quant = scraper(url)
+recipe_name = scraper(url)[0]
+scraped_ingred_quant = scraper(url)[1]
 ingredient_list = cleanup(scraped_ingred_quant)
 
 if __name__ == "__main__":
-    print(ingredient_list)
+    print(recipe_name, ingredient_list)
